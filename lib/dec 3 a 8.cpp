ad2f8c754364bba604c74e331f4cf8dc0f2c1089
Laumaro08 <Laumaro200@gmail.com>
2025-04-16 14:53:16 -0300

    deco

---
+++ lib/dec 3 a 8.cpp
@@ -0,0 +1,7 @@
+DEC3A8.cpp
+#include "DEC3A8.h"
+void Ent_Sal(){
+for(i=0,i<<3,i++){
+    
+}
+}
---
+++ lib/dec3a8.h
@@ -0,0 +1,10 @@
+DEC3A8.h 
+DEC3A8_init();
+DEC3A8(int i0, int i1, int i3);
+#ifdef DEC3A8_h
+#define DECA3A8_h
+#include "Arduino.h"
+int i=0;
+void Ent_Sal();// Entradas y Salidas
+int Lectura(int pin);//leer pin
+#endif

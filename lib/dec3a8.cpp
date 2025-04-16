1c51ca1f65d4a7c432d9c86d59dd4c6d4744bea3
Laumaro08 <Laumaro200@gmail.com>
2025-04-16 16:37:26 -0300

    a

--- lib/dec3a8.cpp
+++ lib/dec3a8.cpp
@@ -1,7 +1,101 @@
-
-#include "DEC3A8.h"
-void DEC3A8_init(){
-for(i=0,i<<3,i++){
-    
-    
-}
+#include "dec3a8.h"
+ int valor, a = 0;
+ void dec3a8_init()
+ {
+     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+ 
+     GPIOB->CRL |= 0x11111111; // config.: 00 : output push-pull and mode: 01 : output max speed 10MHz
+ }
+ void dec3a8(int I0, int I1, int I2)
+ {
+     valor = I0 | I1 << 1 | I2 << 2;
+ 
+     switch (valor)
+     {
+     case 000:
+         for (a = 0; i < 8; a++)
+         {
+             if (a != 0)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 0);
+         break;
+     case 001:
+         for (a = 0; a < 8; a++)
+         {
+             if (i != 1)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 1);
+         break;
+     case 010:
+         for (a = 0; a< 8; a++)
+         {
+             if (a != 2)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 2);
+         break;
+     case 011:
+         for (a = 0; a < 8; a++)
+         {
+             if (i != 3)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 3);
+         break;
+     case 100:
+         for (a = 0; a < 8; a++)
+         {
+             if (a != 4)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 4);
+         break;
+     case 101:
+         for (a = 0; a < 8; a++)
+         {
+             if (a != 5)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR = (1 << 5);
+         break;
+     case 110:
+         for (a = 0; a < 8; a++)
+         {
+             if (i != 6)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 6);
+         break;
+     case 111:
+         for (a = 0; i < 8; a++)
+         {
+             if (i != 7)
+             {
+                 GPIOB->BSRR |= (1 << (a + 16));
+             }
+         }
+         GPIOB->BSRR |= (1 << 7);
+         break;
+     default:
+         GPIOB->BSRR |= 0x00;
+         GPIOB->BSRR |= 0xFF + 0xF;
+         break;
+     }
+ }
--- lib/dec3a8.h
+++ lib/dec3a8.h
@@ -1,14 +1,10 @@
- 
-#ifdef DEC3A8_h
-#define DECA3A8_h
-#include "Arduino.h"
-int i=0, val=0;
-void DEC3A8_init();
-RCC->APB2ENR|=RCC_APB2ENR_IOPBEN;
-RCC->APB2ENR|=RCC_APB2ENR_IOPBEN;
-void DEC3A8(int i0, int i1, int i3);
-/*void Ent_Sal();// Entradas y Salidas
-int Lectura(int pin);//leer pin*/
+#ifndef DEC3A8_H
+#define DEC3A8_H
 
+#include "Arduino.h"
+
+void dec3a8_init();
+
+void dec3a8(int, int, int);
 
 #endif

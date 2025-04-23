dfa9417c827db9aab6ef0dbca0c95ed487985240
Laumaro08 <Laumaro200@gmail.com>
2025-04-23 15:52:14 -0300

    bcd_7seg

---
+++ lib/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "7seg.h"
+void setup(){
+    void _7seg_init();
+}
+void loop(){
+    _7seg(1);
+    delay (2500);
+
+    _7seg(2);
+    delay (2500);
+
+    _7seg(3);
+    delay (2500);
+
+    _7seg(4);
+    delay (2500);
+
+    _7seg(5);
+    delay (2500);
+
+    _7seg(6);
+    delay (2500);
+
+    _7seg(7);
+    delay (2500);
+
+    _7seg(8);
+    delay (2500);
+    
+    _7seg(9);
+    delay (2500);   
+}

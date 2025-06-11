14036c3265eff849dc236ad9c97e228366f8bde1
Laumaro08 <Laumaro200@gmail.com>
2025-06-11 15:09:07 -0300

    CIRCUITO_TERMINADO

---
+++ lib/Multii/DELAY/DELAY.C
@@ -0,0 +1,21 @@
+#include "stm32f103xb.h" 
+
+volatile uint32_t tick_ms = 0;
+
+
+void delayInit(void) {
+    // Configura el SysTick para interrumpir cada 1 ms (72 MHz / 72000 = 1000 Hz)
+    SysTick_Config(SystemCoreClock / 1000);
+}
+
+/* Manejo de timer */
+void SysTick_Handler(void) {
+    if (tick_ms > 0) tick_ms--;
+}
+
+/* Función delay en milisegundos */ 
+void delay_ms(int ms) {
+    delayInit();
+    tick_ms = ms;
+    while (tick_ms);
+}
---
+++ lib/Multii/DELAY/DELAY.H
@@ -0,0 +1,8 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+void delayInit(void);
+void delay_ms(int ms);
+
+
+#endif
